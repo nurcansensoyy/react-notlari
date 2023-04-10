3ea92cadf3e1af0cd03154ab986c4765b4a5d542
nurcansensoyy <nurcansensoyy@gmail.com>
2023-04-10 21:38:35 +0300

    map islemi

--- src/App.js
+++ src/App.js
@@ -2,7 +2,14 @@ import Anasayfa from "./components/Anasayfa";
 import YeniSayfa from "./components/YeniSayfa";
 
 function App() {
+
   const deneme = "props mantigi";
+  const arr = [
+    {name : 'array1', id:0},
+    {name : 'array2', id:1},
+    {name : 'array3', id:2},    
+  ]
+  console.log(arr,"arr");
 
   const clickFunc = () =>{
     console.log("butona tıklandı")
@@ -11,7 +18,10 @@ function App() {
     <div className="App">
     <Anasayfa props = {deneme} />
     <YeniSayfa />
-
+    {arr.map((dizi,i)=> ( //dizide istenen name değerini ekrana bastırmak için map fonksiyonu kullandık.
+      <div key= {i}>{dizi.name}</div> //key = {i} yerine dizi içinde verilen id yi key = {dizi.id} şeklinde kullanabilirdik.
+    )
+    )}
     <button onClick={clickFunc}>Tıkla</button>
     </div>
   );
