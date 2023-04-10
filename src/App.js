9bf0c23f6c906113de5ccdd17937cb1288ac59a8
nurcansensoyy <nurcansensoyy@gmail.com>
2023-04-10 21:50:07 +0300

    useState kullanimi

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import { useState } from "react";
 import Anasayfa from "./components/Anasayfa";
 import YeniSayfa from "./components/YeniSayfa";
 
 function App() {
-
+  const [count, setCount]= useState(0); // burada count 0 ile başlıyor diyoruz. setCount da count u güncelleyeceğimiz fonksiyondur. 
+  // count a string veya obje de verebiliriz
   const deneme = "props mantigi";
   const arr = [
     {name : 'array1', id:0},
@@ -12,12 +14,13 @@ function App() {
   console.log(arr,"arr");
 
   const clickFunc = () =>{
-    console.log("butona tıklandı")
+    setCount(count + 1)
   }
   return (
     <div className="App">
     <Anasayfa props = {deneme} />
     <YeniSayfa />
+    {count}
     {arr.map((dizi,i)=> ( //dizide istenen name değerini ekrana bastırmak için map fonksiyonu kullandık.
       <div key= {i}>{dizi.name}</div> //key = {i} yerine dizi içinde verilen id yi key = {dizi.id} şeklinde kullanabilirdik.
     )
